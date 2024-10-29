e41d9acab0ac5ff0c5b16b27c758666560e16628
윤지현 <139779783+jheony@users.noreply.github.com>
2024-10-29 10:37:22 +0900

    Add files via upload

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,39 +1,174 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-function makeMessage(type, payload){
-    const msg = {type, payload};
-    return JSON.stringify(msg);                             //json을 텍스트 형식으로 변환
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+let roomName;
+let myPeerConnection;
+
+async function getCameras() {
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => device.kind == "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if(currentCamera.label == camera.label){
+                option.selected = true;     // 화면에 보이는 카메라와 선택된 카메라 일치
+            }
+            cameraSelect.appendChild(option);
+        });
+    }catch(e){
+        console.log(e);
+    }
 }
-socket.addEventListener("open", () => {                     //서버 연결
-    console.log("Connected to Server");
-})
-
-socket.addEventListener("message", (message) => {           //배열에 메시지 추가
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-})
-
-socket.addEventListener("close", ()=> {                     //서버 종료
-    console.log("Disconnected from Server");
-})
-
-function handleSubmit(event){
-    event.preventDefault();                                 //기존 이벤트 방지
-    const input = messageForm.querySelector("input");       //메시지 입력칸
-    socket.send(makeMessage("new_message", input.value));
-    input.value= "";                                        //입력칸 초기화
+async function getMedia(deviceId) {
+    const initialConnstraints = {
+        audio: true,
+        video: {facingMode: "user"}     // 모바일 환경 전면카메라 선택
+    };
+    const cameraConstraints = {
+        audio: true,
+        video: {deviceId: {exact: deviceId}}
+    };
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints: initialConnstraints
+        );
+        myFace.srcObject = myStream;
+        if(!deviceId){
+            await getCameras();
+        }  
+    }catch(e){
+        console.log(e);
+    }
 }
 
-function handleNickSubmit(event){
+function handleMuteClick(){     // 뮤트(음소거) 버튼 클릭 시
+    myStream.getAudioTracks().forEach(
+        (track) => (track.enabled = !track.enabled)
+    );
+    if(!muted){    // 토글
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    }else{
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+
+function handleCameraClick(){   // 카메라 버튼 클릭 시
+    myStream.getVideoTracks().forEach(
+        (track) => (track.enabled = !track.enabled)
+    );
+    if(!cameraOff){     // 토글
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }else{
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    }
+}
+
+async function handleCameraChange(){    // 카메라 선택
+    await getMedia(cameraSelect.value);
+    if(myPeerConnection){
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection
+        .getSeders()
+        .find((sender) => sender.track.kind === "video");
+        videoSender.replaceTrack(videoTrack);
+    }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);        
+cameraBtn.addEventListener("click", handleCameraClick);    
+cameraSelect.addEventListener("input", handleCameraChange);
+
+//Welcom Form (join a room)
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+call.hidden = true;
+
+ async function initCall(){  // 입장 후
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+async function handleWelcomeSubmit(event){    // 입장
     event.preventDefault();
-    const input = nickForm.querySelector("input");          //닉네임 입력칸
-    socket.send(makeMessage("nickname", input.value));
-    input.value ="";
+    const input = welcomeForm.querySelector("input");
+    await initCall();
+    socket.emit("join_room", input.value);
+    roomName = input.value;
+    input.value="";
 }
 
-messageForm.addEventListener("submit",handleSubmit);        //메시지 입력 버튼
-nickForm.addEventListener("submit", handleNickSubmit);      //닉네임 입력 버튼
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+socket.on("welcome", async () => {  // offer 송신지 실행
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("sent the offer");
+    socket.emit("offer", offer, roomName);
+})
+
+socket.on("offer", async (offer) => {   // offer 수신지 실행
+    console.log("received the off");
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
+})
+socket.on("answer", answer => {     // offer 송신지에서 answer를 받으면 실행 
+    console.log("received the answer");
+    myPeerConnection.setRemoteDescription(answer);
+})
+socket.on("ice", ice => {
+    console.log("recieved candidate");
+    myPeerConnection.addIceCandidate(ice);
+});
+
+//RTC Code
+function makeConnection(){
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls:[
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302",
+                ]
+            }
+        ]
+    });
+    
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    myStream.getTracks()
+    .forEach(track => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data){
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data){
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();      //서버 생성
@@ -10,28 +10,24 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
-
-const sockets = [];
-
-wss.on("connection", (socket) => {                                      //웹과 연결
-    console.log("Connection to Browser");
-    sockets.push(socket);                                               //메시지 저장
-    socket["nickname"] = "Anonymous"                                    //익명 설정
-    socket.on("close", ()=> console.log("Disconnected from Browser"));  //웹 종료//메시지 출력
-    socket.on("message", (msg) => {          
-        const message = JSON.parse(msg);                       
-        switch(message.type){                                           //닉네임 or 메시지 구분
-            case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`));
-                break;                                                  //"닉네임: 메시지" 출력
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break;
-        }
+wsServer.on("connection", (socket) => {
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice,roomName) => {
+        socket.to(roomName).emit("ice", ice);
     });
 })
-server.listen(3000, handleListen);
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
+httpServer.listen(3000, handleListen);
